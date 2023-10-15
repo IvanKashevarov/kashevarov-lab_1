6350fb51c31aedce485e02a3efd8e689596155db
IvanKashevarov <122292189+IvanKashevarov@users.noreply.github.com>
2023-10-15 19:23:28 +0700

    kasshevarov iv

--- 222.py
+++ 222.py
@@ -2,3 +2,7 @@ import time
 start = time.time()
 n = int(input("Введите верхнюю границу диапазона: "))
 sieve = set(range(2, n+1))
+while sieve:
+    prime = min(sieve)
+    print(prime, end = "\n")
+    sieve -= set(range(prime, n+1, prime))
